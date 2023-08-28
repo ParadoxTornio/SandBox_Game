e542c65375bc7833af0abbcb06c4ec890e111bcb
Paradox <paradox.tornio@gmail.com>
2023-08-28 17:04:35 +0300

    v_13

---
+++ pymunk test.py
@@ -0,0 +1,18 @@
+import pymunk
+
+space = pymunk.Space()
+space.gravity = 0, -981
+
+body = pymunk.Body()
+body.position = 50, 100
+
+
+poly = pymunk.Poly.create_box(body)
+poly.mass = 10
+space.add(body, poly)
+
+print_options = pymunk.SpaceDebugDrawOptions()
+
+for _ in range(100):
+    space.step(0.02)
+    space.debug_draw(print_options)
