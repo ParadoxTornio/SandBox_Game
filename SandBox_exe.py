a75449e67b1dcdaa18ecc1c5dd561cedc5b28a80
Paradox <paradox.tornio@gmail.com>
2023-09-18 17:28:06 +0300

    v_24.2

--- SandBox_exe.py
+++ SandBox_exe.py
@@ -2,7 +2,11 @@ import cx_Freeze
 from gat_all_files import get_files
 all_files = get_files()
 
-executables = [cx_Freeze.Executable('game.py', base='Win32GUI', icon='images/icon.ico')]
+executables = [cx_Freeze.Executable('game.py',
+                                    base='Win32GUI',
+                                    icon='images/icon.ico',
+                                    shortcut_dir='DesktopFolder',
+                                    shortcut_name='SandBox')]
 cx_Freeze.setup(
     name='SandBox', options={'build.exe': {'packages': ['pygame'],
                                            'include_files': all_files}},
